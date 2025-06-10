344c1c1b1f76cd324f530e853371ea3c1ecfd823
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-10 23:29:11 +0200

    Update kitti_detr.py

--- dataset/kitti_detr.py
+++ dataset/kitti_detr.py
@@ -5,26 +5,26 @@ from transformers import DetrForObjectDetection, DetrImageProcessor
 
 # Step 2 for DETR: To create train & val dataset.
 # CocoDetection (parent): Loads image file and raw annotations from JSON.
-# DetrImageProcessor: Preprocesses image (resizes, normalizes), converts boxes and labels into DETR format.
+# DetrImageProcessor: Preprocesses image (resizes, normalizes), converts boxes [cx, cy, w, h] and labels (integer tensors) into DETR format. Returns preprocessed image tensor & List of dictionaries (one per image)
 # KITTIDatasetDETR custom class: Bridges both, uses parent to load raw data, processor to prep for model.
 
 # Set paths
 ANNOTATION_FILE_NAME = "_annotations.coco.json"
 
 # Custom dataset for DETR using COCO-format annotations
-class KITTIDatasetDETR(torchvision.datasets.CocoDetection):
+class KITTIDatasetDETR(torchvision.datasets.CocoDetection):                                             # Custom class: KITTIDatasetDETR, parent class: torchvision.datasets.CocoDetection i.e., extending CocoDetection (reads COCO-style image + annotations)
     def __init__(self, image_directory_path: str, image_processor, train: bool = True):
         annotation_file_path = os.path.join(image_directory_path, ANNOTATION_FILE_NAME)
-        super(KITTIDatasetDETR, self).__init__(image_directory_path, annotation_file_path) # Parent class' constructor
+        super(KITTIDatasetDETR, self).__init__(image_directory_path, annotation_file_path)              # parent class' constructor to set up base functionality (image paths, annotation loading)
         self.image_processor = image_processor
 
     def __getitem__(self, idx):
-        images, annotations = super(KITTIDatasetDETR, self).__getitem__(idx) # Parent class' getitem
+        images, annotations = super(KITTIDatasetDETR, self).__getitem__(idx)                            # parent class' getitem to reuse the logic that reads an image and its annotations
         image_id = self.ids[idx]
         annotations = {'image_id': image_id, 'annotations': annotations}
-        encoding = self.image_processor(images=images, annotations=annotations, return_tensors="pt")
-        pixel_values = encoding["pixel_values"].squeeze()
-        target = encoding["labels"][0]
+        encoding = self.image_processor(images=images, annotations=annotations, return_tensors="pt")    
+        pixel_values = encoding["pixel_values"].squeeze()                                               # to avoid unnecessary extra dimensions  during DataLoader batching
+        target = encoding["labels"][0]                                                                  #  list of dicts, one per image. Only one image (not a batch) is given, so the first and only item with [0] is fetched.
         return pixel_values, target
 
 # Step 3 for DETR: Update the collate function.
